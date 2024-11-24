1b8682b134c8dd3301f88c0d71a6c69dbde04afe
MaximeWeb <neyret.maxime@gmail.com>
2024-11-24 14:59:38 +0100

    newCommit

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -2,6 +2,35 @@
 const photographerHeader = document.querySelector(".photograph-header");
 const mediaSection = document.querySelector(".media_section");   
 const nameForm = document.querySelector(".name-photographe") ; 
+const form = document.querySelector("form");
+const firstName = form.querySelector('input[name="first"]');
+const lastName = form.querySelector('input[name="last"]');
+const email = form.querySelector('input[name="email"]');
+const message = form.querySelector('textarea[name="message"]');
+const formDataElements = form.querySelectorAll(".formData");
+const validated = document.querySelector(".validated p");
+const modal = document.querySelector(".modal");
+const textChanged = document.querySelector(".textwillchange");
+const modalbg = document.querySelector(".bground");
+
+
+const url = "data/photographers.json";  // Je cree une constante avec l'url de mon fichier json , qui va me servir pour fetch
+
+async function fetchData(url) {   // Methode fetch pour recuperer des data 
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    const data = await response.json();     // transforme les data en format json
+    return data;
+  } catch (error) {
+    console.error(error.message);
+    return null; // Pour éviter que la fonction renvoie undefined en cas d'erreur
+  }
+}
+
+
 
 const urlParams = new URLSearchParams(window.location.search);  // URLsearchParam pour analyser l'url 
 const photographerId = urlParams.get('id');                      // .get pour recuperer le parametre choisi , ici l'ID
@@ -11,17 +40,6 @@ async function getPhotographerById(id) {          // ici on vien creer une fonct
   
   return data.photographers.find((photographer) => photographer.id === parseInt(id));   // .find va allez parcourir le tableau jusqu'a trouvé un ID
 }
-
-
-// async function getLikesData() {
-//   const data = await fetchData(url);
-//   const filteredData = data.media.filter((media) => media.likes > 0);
-//   const sortedData = filteredData.sort((a, b) => b.likes - a.likes);
-//   console.log(sortedData);
-  
-//   return sortedData; // Retourne les données triées si nécessaire
-// }
-// getLikesData()
 
 // Afficher les informations du photographe
 async function displayPhotographerData() {                            // Maintenant on va afficher les info du photographe en fonction de l'id
@@ -39,10 +57,13 @@ async function displayPhotographerData() {                            // Mainten
   nameForm.appendChild(photographerDOM);
 }
 
+displayPhotographerData();
+
+
+
 async function getMediaByPhotographerId(photographerId) {       // ici on vien creer une fonction qui va recupérer des media via l'id du photographer
   const data = await fetchData(url);
    return data.media.filter((media) => media.photographerId === parseInt(photographerId));  // on utilise filter car il y'a plusieurs element a recuperer
-
 }
 
 function buildImageLink(media, photographerName) {    // je créer une fonction pour recupérer les images qui ce trouve dans un dossier asset   
@@ -53,7 +74,7 @@ function buildImageLink(media, photographerName) {    // je créer une fonction 
 async function displayMedia(sortedMedia) {
   const photographer = await getPhotographerById(photographerId); // Récupère les infos du photographe
   const stringName = photographer.name.toLowerCase().replace(" ", ""); // Adapter le nom pour le chemin du dossier
-  
+
   mediaSection.innerHTML = "";
 
   sortedMedia.forEach((media) => {
@@ -62,8 +83,8 @@ async function displayMedia(sortedMedia) {
     const mediaCardDOM = mediaModel.mediaDOM(); // Génère le DOM pour chaque média
     mediaSection.appendChild(mediaCardDOM); // Ajoute au conteneur des médias
   });
-}
 
+}
 
 
 // function pour trié en function de la categorie
@@ -74,13 +95,16 @@ async function modifCategories(event) {
   let sortedMedia;
   switch (categorie) {
     case "popularité":
-      sortedMedia = mediaItems.sort((a, b) => b.likes - a.likes); //  likes décroissant
+      sortedMedia = mediaItems.sort((a, b) => b.likes - a.likes); 
+      console.log(mediaItems)//  likes décroissant
       break;
     case "date":
-      sortedMedia = mediaItems.sort((a, b) => new Date(b.date) - new Date(a.date)); //date décroissante
+      sortedMedia = mediaItems.sort((a, b) => new Date(b.date) - new Date(a.date));
+      console.log(mediaItems) //date décroissante
       break;
     case "titre":
-      sortedMedia = mediaItems.sort((a, b) => a.title.localeCompare(b.title)); // Tri par ordre alphabetique
+      sortedMedia = mediaItems.sort((a, b) => a.title.localeCompare(b.title));
+      console.log(mediaItems) // Tri par ordre alphabetique
       break;
     default:
   }
@@ -89,20 +113,231 @@ async function modifCategories(event) {
   displayMedia(sortedMedia);
 }
 
+
+let mediaItems = []; // Pour stocker les médias liés au photographe
+
+let currentMediaIndex = 0;
+
+async function switchPicModal(direction) {
+  if (mediaItems.length === 0) {
+    mediaItems = await getMediaByPhotographerId(photographerId);
+     // on vien recuperer tous les media du photographer in a array.
+  }
+
+  if (direction === "next") {
+    currentMediaIndex = (currentMediaIndex + 1) % mediaItems.length;
+  } else if (direction === "previous") {
+    currentMediaIndex = (currentMediaIndex - 1 + mediaItems.length) % mediaItems.length;
+  }
+
+  const currentMedia = imageLink[currentMediaIndex];
+  const photographer = await getPhotographerById(photographerId);
+  const stringName = photographer.name.toLowerCase().replace(" ", "");
+  const mediaLink = buildImageLink(currentMedia, stringName);
+ 
+  return console.log(mediaLink, currentMedia.title) 
+}
+
+const arrowLeft = document.querySelector(".left");
+const arrowRight = document.querySelector(".right");
+
+arrowLeft.addEventListener("click", () => switchPicModal("previous"));
+arrowRight.addEventListener("click", () => switchPicModal("next"));
+
+
+
 // Evenement Change 
 const menuSelect = document.getElementById("menu-select");
 menuSelect.addEventListener("change", modifCategories);
 
-// Chargement initial des médias triés par popularité et affiche avec displayMedia
+// Affichage initial des médias triés par popularité et affiche avec displayMedia
 async function initialDisplayMedia() {
   const mediaItems = await getMediaByPhotographerId(photographerId);
+  console.log(mediaItems)
   const sortedByPopularity = mediaItems.sort((a, b) => b.likes - a.likes);
   displayMedia(sortedByPopularity);
 }
 
 initialDisplayMedia();
-displayPhotographerData();
-// displayMedia();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// FORMULAIRE 
+
+const originalTextChanged = textChanged.textContent;
+
+
+
+function showError(input, message) {
+  // fonction afin de faire apparaitre les messages d'erreur avec la classe css formData
+  const formData = input.closest(".formData"); // on va chercher l'element le plus proche du parent formData , donc les input .
+  formData.setAttribute("data-error", message); // setAttribute afin de recuperer l'attribut data-error + ajouté un message d'erreur
+  formData.setAttribute("data-error-visible", "true"); // ici on va faire apparaitre l'emplacement du message d'erreur
+}
+
+function hideError(input) {
+  // On va gérer le cas ou l'utilisateur est revenu sur le champ afin de modifier son erreur
+  const formData = input.closest(".formData");
+  formData.setAttribute("data-error-visible", "false");
+  formData.setAttribute("data-error", "");
+}
+
+function hideAllErrors() {
+  formDataElements.forEach((formData) => {
+    formData.setAttribute("data-error-visible", "false"); // Cache le message d'erreur
+    formData.setAttribute("data-error", ""); // Efface le texte du message d'erreur
+  });
+}
+
+function displayModal() {
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "block";
+  modalbg.style.display = "block";
+}
+// Ferme le formulaire
+function closeModal() {
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+  form.classList.remove("hidden");
+  hideAllErrors();
+
+  textChanged.textContent = originalTextChanged;    // Mrssage de validation . 
+  textChanged.style.fontSize = "64px";
+}
+
+const validate = (event) => {
+  event.preventDefault(); 
+  let isValid = true; 
+
+  const nameRegex = /^[a-zA-ZÀ-ÿ '-]{2,}$/; 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
+
+  const formData = new FormData(form); // Je crée un nouvelle objet iterable FormData qui aura comme param Key,Value
+  
+  for (const [key, value] of formData.entries()) {
+    // for of pour agir sur plusieur champs sous forme de boucle avec les param key,value
+    switch (
+      key // switch pour eviter les répétition if if if avec le param key
+    ) {
+      case "first": // case qui fonctionne avec switch " dans le cas ou la clef s'apelle first ..ect"
+        if (!nameRegex.test(value.trim())) {
+          // .trim pour que les espace ne soit pas compter comme des characteres , .test methode de verification du regex
+          showError(
+            firstName,
+            "Veuillez entrer un prénom valide (2 caractères minimum)."
+          ); // fonction showError + input + message
+          isValid = false;
+        } else {
+          hideError(firstName);
+        }
+        break;
+
+      case "last":
+        if (!nameRegex.test(value.trim())) {
+          showError(
+            lastName,
+            "Veuillez entrer un nom valide (2 caractères minimum)."
+          );
+          isValid = false;
+        } else {
+          hideError(lastName);
+        }
+        break;
+
+      case "email":
+        if (!emailRegex.test(value.trim())) {
+          showError(email, "Veuillez entrer une adresse e-mail valide.");
+          isValid = false;
+        } else {
+          hideError(email);
+        }
+        break;
+
+      case "message":
+        if (value.length < 2 || value.length > 150) {
+          showError(message, "Veuillez ecrire un message ");
+          isValid = false;
+        } else {
+          hideError(message);
+        }
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  if (isValid) {
+    form.reset();
+    form.classList.add("hidden");
+    textChanged.textContent = "Votre formulaire a été envoyé a ";
+    textChanged.style.fontSize = "22px";
+  }
+};
+
+
+document.querySelector(".closeModal").addEventListener("click", () => {
+  closeModal();
+});
+
+
+
+// Submit formulaire
+form.addEventListener("submit", validate);
+
+
+
 
 
 
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,44 +1,8 @@
-const form = document.querySelector("form");
-const firstName = form.querySelector('input[name="first"]');
-const lastName = form.querySelector('input[name="last"]');
-const email = form.querySelector('input[name="email"]');
-const message = form.querySelector('textarea[name="message"]');
-const formDataElements = form.querySelectorAll(".formData");
-const validated = document.querySelector(".validated p");
 const lightbox = document.querySelector(".lightbox");
-const modal = document.querySelector(".modal");
-const textChanged = document.querySelector(".textwillchange");
-const modalbg = document.querySelector(".bground");
 const lightboxbg = document.querySelector(".bgroundLightbox");
 const buttonCloseLightbox = document.querySelector(".closeLightbox");
-const arrowLeft = document.querySelector(".left");
-const arrowRight = document.querySelector(".right");
 
-const originalTextChanged = textChanged.textContent;
-
-
-
-function showError(input, message) {
-  // fonction afin de faire apparaitre les messages d'erreur avec la classe css formData
-  const formData = input.closest(".formData"); // on va chercher l'element le plus proche du parent formData , donc les input .
-  formData.setAttribute("data-error", message); // setAttribute afin de recuperer l'attribut data-error + ajouté un message d'erreur
-  formData.setAttribute("data-error-visible", "true"); // ici on va faire apparaitre l'emplacement du message d'erreur
-}
-
-function hideError(input) {
-  // On va gérer le cas ou l'utilisateur est revenu sur le champ afin de modifier son erreur
-  const formData = input.closest(".formData");
-  formData.setAttribute("data-error-visible", "false");
-  formData.setAttribute("data-error", "");
-}
-
-function hideAllErrors() {
-  formDataElements.forEach((formData) => {
-    formData.setAttribute("data-error-visible", "false"); // Cache le message d'erreur
-    formData.setAttribute("data-error", ""); // Efface le texte du message d'erreur
-  });
-}
-
+  
 // Ouvre la lightbox
 function displayLightbox() {
   lightbox.style.display = "block";
@@ -49,25 +13,11 @@ function closeLightbox() {
   lightbox.style.display = "none";
   lightboxbg.style.display = "none";
 }
-// Fait apparaitre le formulaire
-function displayModal() {
-  const modal = document.getElementById("contact_modal");
-  modal.style.display = "block";
-  modalbg.style.display = "block";
-}
-// Ferme le formulaire
-function closeModal() {
-  const modal = document.getElementById("contact_modal");
-  modal.style.display = "none";
-  form.classList.remove("hidden");
-  hideAllErrors();
 
-  textChanged.textContent = originalTextChanged;    // Mrssage de validation . 
-  textChanged.style.fontSize = "64px";
-}
 
 function photographerTemplate(data) {
   const { name, portrait, city, country, tagline, price, id } = data;
+  
 
   const picture = `assets/PhotographersIDPhotos/${portrait}`;
   // INDEX.HTML     PHOTOS DE PROFIL + INFOS DES PHOTOGRAPHES
@@ -120,6 +70,13 @@ function photographerTemplate(data) {
 
 function mediaTemplate(data) {
   const { name, imageLink, title, likes } = data;
+ 
+  const array = [ name, imageLink, title, likes ]
+
+  ;
+  // console.log(array)
+ 
+  
   let currentLikes = likes;
 
   const isVideo = imageLink.endsWith(".mp4"); // isVideo sera un fichier qui fini par mp4
@@ -148,54 +105,31 @@ function mediaTemplate(data) {
           </div>
         </div>
       `;
-
+      
     const likeIcon = article.querySelector(".likeIcon");
     const likesCount = article.querySelector(".likesNumbers");
     const imgMedia = article.querySelector(".media"); 
     const videoMedia = article.querySelector(".videoMedia video"); 
-   
-   
-        // Gere le contenue de la lightbox
-    function showMediaInLightbox(media, title ) {
-      
-      if (isVideo) {
-        lightbox.innerHTML = `
-          <video controls>
-            <source src="${media}" type="video/mp4">
-          </video>
-          <p>${title}</p>
-        `;
-      } else {
-        lightbox.innerHTML = `
-          <img src="${media}" alt="${title}">
-          <p>${title}</p>
-        `;
-      }
-    }
-        // Ajoute l'evenement d'ouverture de la Modale et indique le media de showmedia
+
+
     if (imgMedia) {
-      imgMedia.addEventListener("click", () => {
-        displayLightbox(); 
-        showMediaInLightbox(imageLink, title); 
-      });
-    }
+        imgMedia.addEventListener("click", () => {
+          displayLightbox(); 
+          showMediaInLightbox(); 
+          // console.log(imageLink)
+        });
+      }
+  
+      if (videoMedia) {
+        videoMedia.addEventListener("click", () => {
+          displayLightbox(); 
+          showMediaInLightbox();
+          // console.log(imageLink)
+        });
+      }
 
-    if (videoMedia) {
-      videoMedia.addEventListener("click", () => {
-        displayLightbox(); 
-        showMediaInLightbox(imageLink, title);
-      });
-    }
-   
-
-    buttonCloseLightbox.addEventListener("click", () => closeLightbox())
-
-    
-    // arrowLeft.addEventListener("click", () => navigateLightbox("prev"));
-    // arrowRight.addEventListener("click", () => navigateLightbox("next"));
-
-
-    likeIcon.addEventListener("click", () => {
+  // LIKES //
+        likeIcon.addEventListener("click", () => {
       // event au click de l'icon heart , on ajouté +1 au click et -1 si on reclik
       if (likeIcon.classList.contains("fa-regular")) {
         // on change l'icon en fonction du click
@@ -210,91 +144,36 @@ function mediaTemplate(data) {
       likesCount.textContent = currentLikes;
     });
 
+    
     return article;
-  }
+    }
+        // Gere le contenue de la lightbox
+    function showMediaInLightbox() {
+    lightbox.innerHTML = `
+      ${
+        isVideo
+          ? `
+          <video controls>
+               <source  src="${imageLink}" type="video/mp4">
+             </video>
+             </div>
+             ` // sinon on affiche la div image
+          : `<img class="media" src="${imageLink}" alt="Photo de ${name}">`
+      }`;
+    
+    }
+   
+    //     // Ajoute l'evenement d'ouverture de la Modale et indique le media de showmedia
+    //
+   
 
+    buttonCloseLightbox.addEventListener("click", () => closeLightbox())
+
+    
+    // arrowLeft.addEventListener("click", () => navigateLightbox("prev"));
+    // arrowRight.addEventListener("click", () => navigateLightbox("next"));
+  
   return { mediaDOM };
 }
 
 
-// FORMULAIRE 
-
-const validate = (event) => {
-  event.preventDefault(); 
-  let isValid = true; 
-
-  const nameRegex = /^[a-zA-ZÀ-ÿ '-]{2,}$/; 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; 
-
-  const formData = new FormData(form); // Je crée un nouvelle objet iterable FormData qui aura comme param Key,Value
-  const data = Object.fromEntries(formData.entries()); // Grace a .entries j'obtiens un iterateur et fromEntries pour le changer en objet js lisible
-  console.log(data);
-
-  for (const [key, value] of formData.entries()) {
-    // for of pour agir sur plusieur champs sous forme de boucle avec les param key,value
-    switch (
-      key // switch pour eviter les répétition if if if avec le param key
-    ) {
-      case "first": // case qui fonctionne avec switch " dans le cas ou la clef s'apelle first ..ect"
-        if (!nameRegex.test(value.trim())) {
-          // .trim pour que les espace ne soit pas compter comme des characteres , .test methode de verification du regex
-          showError(
-            firstName,
-            "Veuillez entrer un prénom valide (2 caractères minimum)."
-          ); // fonction showError + input + message
-          isValid = false;
-        } else {
-          hideError(firstName);
-        }
-        break;
-
-      case "last":
-        if (!nameRegex.test(value.trim())) {
-          showError(
-            lastName,
-            "Veuillez entrer un nom valide (2 caractères minimum)."
-          );
-          isValid = false;
-        } else {
-          hideError(lastName);
-        }
-        break;
-
-      case "email":
-        if (!emailRegex.test(value.trim())) {
-          showError(email, "Veuillez entrer une adresse e-mail valide.");
-          isValid = false;
-        } else {
-          hideError(email);
-        }
-        break;
-
-      case "message":
-        if (value.length < 2 || value.length > 150) {
-          showError(message, "Veuillez ecrire un message ");
-          isValid = false;
-        } else {
-          hideError(message);
-        }
-        break;
-
-      default:
-        break;
-    }
-  }
-
-  if (isValid) {
-    form.reset();
-    form.classList.add("hidden");
-    textChanged.textContent = "Votre formulaire a été envoyé a ";
-    textChanged.style.fontSize = "22px";
-  }
-};
-
-
-document.querySelector(".closeModal").addEventListener("click", () => {
-  closeModal();
-});
-
-// Submit formulaire
-form.addEventListener("submit", validate);
